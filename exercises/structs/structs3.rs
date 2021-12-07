110f1cc3108abd891b4efeba6f6554a570510806
Seongheum Choi <kingman330@gmail.com>
2021-12-07 01:57:27 +0000

    SOLVE Chapter structs

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -16,7 +16,7 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // Something goes here...
+            panic!("Weightless Package");
         } else {
             Package {
                 sender_country,
@@ -26,12 +26,16 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        if self.sender_country != self.recipient_country {
+            true
+        } else {
+            false
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        self.weight_in_grams * cents_per_gram
     }
 }
 
